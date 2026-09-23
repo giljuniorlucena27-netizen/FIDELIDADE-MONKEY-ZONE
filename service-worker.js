6e51afe1a2c521f03e21b5615964ed3947ec0d5c
giljuniorlucena27-netizen <giljuniorlucena27@gmail.com>
2026-09-22 22:35:36 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'fidelidade-monkey-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
+  );
+});
